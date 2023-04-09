1cc1b79a81a96b1c6fea6de7de09e1a22fa59246
= <MaxsoftLtd>
2023-04-09 13:17:25 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [text, setText] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = (todo) => {
+		if (!todo) return;
+
+		setTodos((prevItems) => {
+			return [...prevItems, todo];
+		});
+
+		setText("");
+	};
+
+	const deleteItem = (id) => {
+		setTodos((prevItems) => {
+			return prevItems.filter((_, index) => {
+				return index !== id;
+			});
+		});
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Todo List App!</Text>
+			<TextInput
+				style={styles.input}
+				placeholderTextColor={"white"}
+				onChangeText={setText}
+				value={text}
+				placeholder={"Please type here…"}
+			/>
+			<Button
+				title="Add Todo"
+				disabled={!text}
+				onPress={() => addTodo(text)}
+			/>
+
+			{todos.map((todo, index) => (
+				<Text
+					key={index}
+					style={styles.todo}
+					onPress={() => deleteItem(index)}
+				>
+					{index + 1}. {todo}
+				</Text>
+			))}
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#393646",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		color: "#fff",
+		fontSize: 20,
+	},
+	input: {
+		padding: 16,
+		marginTop: 50,
+		color: "#fff",
+	},
+	todo: {
+		padding: 10,
+		color: "#FF8400",
+		fontWeight: "bold",
+	},
 });
